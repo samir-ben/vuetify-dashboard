eb62ef630164ee0dc33754f4726621c942727b6e
samir-ben <samir-69@hotmail.fr>
2019-01-25 02:53:47 +0100

    feat: card with data in store
    - created card element
    - add data in store
    - connect projects component with state (vuex)
    - add some style to the projects component

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,60 @@
+
 <template>
-  <div class="projects">
-    <h1>Projects</h1>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+      
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>{{ project.status }}</div>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
+
+    </v-container>
    
   </div>
 </template>
 
 <script>
-
-
-  export default {
-
-  }
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapState({
+    projects: state => state.projects
+  })
+  },
+}
 </script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+</style>
