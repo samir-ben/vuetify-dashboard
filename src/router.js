759becf8fe6c8fb7e63fab53940b5d7e9be59514
samir-ben <samir-69@hotmail.fr>
2019-01-26 02:35:17 +0100

    feat: team component
    - created a team component
    - add a card vuetify component
    - add mapgetters method to get data in team store
    - iterating method to display each personn of the team

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from './views/Dashboard.vue';
 import Projects from './views/Projects.vue';
+import Team from './views/Team.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/projects',
       name: 'projects',
       component: Projects
+    },
+    {
+      path: '/team',
+      name: 'team',
+      component: Team
     }
   ]
 });
