2925d4920d436a1b4ba995e53cb43a8beb598170
samir-ben <samir-69@hotmail.fr>
2019-01-26 02:36:53 +0100

    feat: counting projetc
    - get data in projets store
    - displaying count by project status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="dashboard">
     <h1>Dashboard</h1>
+    <p>Total projet : {{totalProjects}}</p>
+    <p>Total projet fini :{{totalProjectsComplete}}</p>
+    <p>Total projet en cours :{{totalProjectsOnGoing}}</p>
+    <p>total projet en retard : {{totalProjectsOverDue}}</p>
    
   </div>
 </template>
 
 <script>
-
-
+import { mapGetters } from "vuex";
   export default {
-
+    name: 'Dashboard',
+    computed: {
+    // rajouter les accesseurs dans `computed` avec l'opérateur de décomposition
+    ...mapGetters('projects', ['totalProjects', 'totalProjectsComplete', 'totalProjectsOnGoing', 'totalProjectsOverDue'])
+  }
   }
 </script>
