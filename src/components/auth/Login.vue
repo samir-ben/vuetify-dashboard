cfd3636898d311fbd283e5da2988f1a66df94cc4
samir-ben <samir-69@hotmail.fr>
2019-01-28 19:55:49 +0100

    feat: form components
    - add a login component with a form
    - add a signup component with a form
    - add a form adding project component
    - config router with new components

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login container">
+    <v-layout row wrap justify-space-around>
+      <v-flex xs6>
+    <v-form class="px-3 mt-5" ref="form">
+
+            <h1>Se connecter</h1>
+          <v-text-field v-model="email" label="Email" ></v-text-field>
+          <v-text-field v-model="password" label="Mot de passe"></v-text-field>
+
+          <v-spacer></v-spacer>
+
+          <v-btn flat class="success mx-0 mt-3" >Se connecter</v-btn>
+        </v-form>
+        </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return{
+      email: null,
+      password: null,
+      feedback: null
+    }
+  },
+  methods: {
+    login(){
+     
+    }
+  }
+}
+</script>
+
+<style>
+form {
+    padding: 20px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 import Dashboard from './views/Dashboard.vue';
 import Projects from './views/Projects.vue';
 import Team from './views/Team.vue';
+import Login from './components/auth/Login.vue';
+import Signup from './components/auth/Signup.vue';
 
 Vue.use(Router);
 
@@ -24,6 +26,16 @@ export default new Router({
       path: '/team',
       name: 'team',
       component: Team
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 });
