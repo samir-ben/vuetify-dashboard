8cc516ac605ac06d8b4409b569828b19dd8f46b9
samir-ben <samir-69@hotmail.fr>
2019-01-28 16:37:05 +0100

    feat: projetcs getters
    - add a getter for label chart bar name
    - add a getter for totalcompletebyauthor array
    - add a getter with method to get a new array with each person & status

--- src/store/projects.js
+++ src/store/projects.js
@@ -1,15 +1,21 @@
 const state = {
   projects: [
-    { title: 'Design a new website by Sam', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+    { title: 'Design a new website by Sam', person: 'The Net Ninja', due: '1st Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
     { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
     { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+    { title: 'Design by Sam', person: 'The Net Ninja', due: '1st Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
     { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
     { title: 'Create an application', person: 'Gouken', due: '20th Oct 2018', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+    { title: 'application', person: 'Jeremy', due: '20th Oct 2018', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+    { title: 'Design a new  by Sam', person: 'The Net Ninja', due: '1st Jan 2019', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+    { title: 'the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
   ],
+  totalCompleteByAuthor : []
 };
 
 const getters = {
   projects: state => state.projects,
+  totalCompleteByAuthor: state => state.totalCompleteByAuthor,
   totalProjects: state => state.projects.length,
   totalProjectsComplete: state => {
     let i = state.projects.filter(todo => todo.status == 'complete');
@@ -22,7 +28,33 @@ const getters = {
   totalProjectsOverDue: state => {
     let i = state.projects.filter(todo => todo.status == 'overdue');
     return i.length;
-  }
+  },
+  nameLabelChart: state => {
+    const label = [];
+    state.projects.forEach(name => {
+      label.push(name.person);
+    });
+    let i = label.filter(function (item, pos) {
+      return label.indexOf(item) == pos;
+    })
+    return i
+  },
+  totalProjectsByPersonAndStatus: state => {
+    let array = state.projects.reduce((output, { person, status }) => {
+      if (output[person]) {
+        output[person][status]++
+      } else {
+        output[person] = {
+          person,
+          complete: Number(status === 'complete'),
+          ongoing: Number(status === 'ongoing'),
+          overdue: Number(status === 'overdue')
+        }
+      }
+      return output;
+    }, {})
+     return Object.values(array)
+  },
 };
 
 const mutations = {
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="dashboard">
-   <vc-donut
-    background="white" foreground="grey"
-    :size="200" unit="px" :thickness="30"
-    hasLegend legendPlacement="top"
-    :sections="projetcsDataGraph" :total="this.totalProjects"
-  >
-    <h1>100%</h1>
-  </vc-donut>
-  <vc-donut
-    background="white" foreground="grey"
-    :size="200" unit="px" :thickness="30"
-    hasLegend legendPlacement="top"
-    :sections="teamDataGraph" :total="this.totalTeam"
-  >
-    <h1>Team</h1>
-  </vc-donut>
+     <v-layout row wrap>
+        <v-flex xs12 class="mt-5">
+          <vue-frappe
+            v-if="this.label"
+            id="test"
+            :labels="this.label"
+            title="Avancement des projets"
+            type="bar"
+            :height="300"
+            :colors="['#3CB371', '#1E90FF', '#FF6347']"
+            :dataSets="this.data">
+          </vue-frappe>
+        </v-flex>
 
-    <h1>Dashboard</h1>
-    <p>Total projet : {{totalProjects}}</p>
-    <p>Total projet fini :{{totalProjectsComplete}}</p>
-    <p>Total projet en cours :{{totalProjectsOnGoing}}</p>
-    <p>total projet en retard : {{totalProjectsOverDue}}</p>
+        <v-flex xs12 sm12 md6 class="mt-5 border-right">
+          <vc-donut
+          background="white" foreground="grey"
+          :size="200" unit="px" :thickness="30"
+          hasLegend legendPlacement="top"
+          :sections="projetcsDataGraph" :total="this.totalProjects">
+            <h1>100%</h1>
+        </vc-donut>
+        </v-flex>
 
-    <p>Total projet : {{totalTeam}}</p>
-    <p>Total projet fini :{{totalManager}}</p>
-    <p>Total projet en cours :{{totalFrontDeveloper}}</p>
-    <p>total projet en retard : {{totalBackDeveloper}}</p>
-    <p>total projet en retard : {{totalDesigner}}</p>
-   
+        <v-flex xs12 sm12 md6 class="mt-5">
+          <vc-donut
+          background="white" foreground="grey"
+          :size="200" unit="px" :thickness="30"
+          hasLegend legendPlacement="top"
+          :sections="teamDataGraph" :total="this.totalTeam">
+          <h1>Team</h1>
+        </vc-donut>
+        </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -36,41 +41,77 @@
 import { mapGetters } from "vuex";
 import 'vue-css-donut-chart/dist/vcdonut.css';
 
-  export default {
-    name: 'Dashboard',
-    data() {
-      return {
-        projetcsDataGraph: [
-          { label: 'Total projet fini', value: 0, color: 'LightGreen' },
-          { label: 'Total projet en cours', value:0 , color: 'DeepSkyBlue' },
-          { label: 'total projet en retard', value:0, color: 'Crimson' }
-        ],
-        teamDataGraph: [
-          { label: 'Manager', value: 0, color: 'MediumPurple' },
-          { label: 'Développeur Frontend', value:0 , color: 'MediumAquaMarine' },
-          { label: 'Développeur Backend', value:0, color: 'LightSteelBlue' },
-          { label: 'Designer', value:0, color: 'Crimson' }
-        ]
-      };
-    },
-    mounted() {
-      this.projetcsDataGraph[0].value = this.totalProjectsComplete;
-      this.projetcsDataGraph[1].value = this.totalProjectsOnGoing;
-      this.projetcsDataGraph[2].value = this.totalProjectsOverDue;
-      this.teamDataGraph[0].value = this.totalManager;
-      this.teamDataGraph[1].value = this.totalFrontDeveloper;
-      this.teamDataGraph[2].value = this.totalBackDeveloper;
-      this.teamDataGraph[3].value = this.totalDesigner;
-    },
-    computed: {
-    // rajouter les accesseurs dans `computed` avec l'opérateur de décomposition
-    ...mapGetters(
-    'projects', ['totalProjects', 'totalProjectsComplete', 'totalProjectsOnGoing', 'totalProjectsOverDue'],
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      label : [],
+      projetcsDataGraph: [
+        { label: 'Total projet fini', value: 0, color: 'MediumSeaGreen' },
+        { label: 'Total projet en cours', value:0 , color: 'DodgerBlue' },
+        { label: 'total projet en retard', value:0, color: 'Tomato' }
+      ],
+      teamDataGraph: [
+        { label: 'Manager', value: 0, color: 'Khaki' },
+        { label: 'Développeur Frontend', value:0 , color: 'MediumTurquoise' },
+        { label: 'Développeur Backend', value:0, color: 'Tan' },
+        { label: 'Designer', value:0, color: 'Plum' }
+      ],
+      data: [
+        { name: "Terminé", chartType: 'bar',values: [] },
+        { name: "En cours", chartType: 'bar', values: [] },
+        { name: "En retard", chartType: 'bar', values: [] }
+      ],
+    };
+  },
+  created() {
+  // Add data from getters on donuts chart
+  this.projetcsDataGraph[0].value = this.totalProjectsComplete;
+  this.projetcsDataGraph[1].value = this.totalProjectsOnGoing;
+  this.projetcsDataGraph[2].value = this.totalProjectsOverDue;
+  this.teamDataGraph[0].value = this.totalManager;
+  this.teamDataGraph[1].value = this.totalFrontDeveloper;
+  this.teamDataGraph[2].value = this.totalBackDeveloper;
+  this.teamDataGraph[3].value = this.totalDesigner;
+  // add data from getters to bar charts
+  this.getCompleteByAuthor();
+  this.getLabel()
+  },
+  computed: {
+  ...mapGetters(
+  'projects', ['projects', 'totalProjects', 'totalProjectsComplete', 'totalProjectsOnGoing', 'totalProjectsOverDue', 'projectStatusByAuthor', 'completed', 'totalCompleteByAuthor', 'totalProjectsByPersonAndStatus', 'nameLabelChart'],
 
-     ),
-     ...mapGetters(
-    'team', ['totalTeam','totalManager', 'totalFrontDeveloper', 'totalBackDeveloper', 'totalDesigner']
-     )
-  }
-  }
+    ),
+    ...mapGetters(
+  'team', ['totalTeam','totalManager', 'totalFrontDeveloper', 'totalBackDeveloper', 'totalDesigner']
+    )
+  },
+  methods: {
+    getCompleteByAuthor(){
+      const array1 = [];
+      const array2 = [];
+      const array3 = [];
+      this.totalProjectsByPersonAndStatus.forEach(x => {
+        array1.push(x.complete);
+        array2.push(x.ongoing);
+        array3.push(x.overdue);
+      });
+      this.data[0].values = array1;
+      this.data[1].values = array2;
+      this.data[2].values = array3;
+    },
+    getLabel(){
+      this.totalProjectsByPersonAndStatus.forEach(x => {
+        this.label.push(x.person);
+      });
+    }
+  },
+}
 </script>
+
+<style scoped>
+.border-right{
+  border-right: 1px solid rgb(196, 196, 196);
+}
+</style>
+
