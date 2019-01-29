1fda13df32b826801e64f70ed786a7c2bab004b1
samir-ben <samir-69@hotmail.fr>
2019-01-30 00:35:05 +0100

    style: add some style

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,7 +4,7 @@
     <v-container>
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
-          <v-card flat class="text-xs-center ma-3">
+          <v-card flat class="text-xs-center ma-3 elevation-2">
             <v-responsive class="pt-4">
               <v-avatar size="100" class="grey lighten-2">
                 <img class="avatar" :src="person.avatar">
@@ -14,10 +14,10 @@
               <div class="subheading">{{ person.name }}</div>
               <div class="grey--text">{{ person.role }}</div>
             </v-card-text>
-            <v-card-actions>
+            <v-card-actions class="">
               <v-btn flat color="grey">
-                <v-icon small left>message</v-icon>
-                <span class="">Message</span>
+                <v-icon small >info</v-icon>
+                <span class="">Info</span>
               </v-btn>
             </v-card-actions>
           </v-card>
